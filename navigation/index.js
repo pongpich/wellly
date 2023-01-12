7fa841c05cad249d4e7903e4597a87a786a6751d
THANETNove <Novel_Manga@outlook.co.th>
2023-01-12 16:54:15 +0700

    upcode

--- navigation/index.js
+++ navigation/index.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react'
 import { Button, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator,CardStyleInterpolators } from '@react-navigation/stack';
+import { createStackNavigator,CardStyleInterpolators,TransitionSpecs } from '@react-navigation/stack';
 import Login from '../screens/Login';
 import ForgotPassword from '../screens/ForgotPassword';
 import Walkthrough from '../screens/Walkthrough';
 
 
 const Stack = createStackNavigator();
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
 
 function MyStack() {
   const navigation = useNavigation();
+  
   return (
-    <Stack.Navigator>
+    <Stack.Navigator 
+    screenOptions={{
+      headerTintColor: "#3762FC", // ใส่ icon สี ปุ่ม BackTitle
+    }}>
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
      <Stack.Screen name="ForgotPassword" component={ForgotPassword}  options={{
         title: "",
+        //headerBackTitle: true, //ซ่อนข้อความในของ ios
+        headerShadowVisible: false, // applied here
         headerLeft: () => (
-          <View>
+          <View style={{paddingLeft: 15}}>
             <TouchableOpacity onPress={() => navigation.goBack()}>
             <Image
             source={require('../assets/images/icon/caret.png')}
