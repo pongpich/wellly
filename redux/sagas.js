28686cdb67ef2ea1b05024b90172a096ecb7d900
THANETNove <Novel_Manga@outlook.co.th>
2023-01-15 21:20:02 +0700

    ลบ ตัว sagas

--- redux/sagas.js
+++ redux/sagas.js
@@ -1,11 +1,9 @@
 import { all } from 'redux-saga/effects';
 import { saga as authSagas } from './auth';
-import { saga as personalDataUserSagas } from './personalUser';
 
 
 export default function* rootSaga(getState) {
   yield all([
     authSagas(),
-    personalDataUserSagas(),
   ]);
 }
