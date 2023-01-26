532cbaca068efc71a01e0be53ae970cbf0936b43
THANETNove <Novel_Manga@outlook.co.th>
2023-01-26 09:23:36 +0700

    เพิ่ม 2 ภาษา หน้า Forgot password

--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import { Alert, Modal, SafeAreaView, StyleSheet, Text, Image, Linking, Dimensions, Pressable, View } from "react-native";
 import colors from '../constants/colors';
 import ComponentsStyle from '../constants/components';
+import i18next from 'i18next';
+import { withTranslation } from 'react-i18next';
 
 class ForgotPassword extends Component {
+
   render() {
+    const { t } = this.props;
     return (
       <SafeAreaView style={ComponentsStyle.containerWhite}>
         <View style={ComponentsStyle.viewStyle}>
@@ -14,8 +18,8 @@ class ForgotPassword extends Component {
                 style={styles.entryImage}
                 source={require('../assets/images/icon/contextual.png')}
               />
-              <Text style={styles.textForgotPass}>ลืมรหัสผ่าน</Text>
-              <Text style={styles.textForgotPass_2}>ติดต่อฝ่ายบุคคลบริษัทของคุณเพื่อทำการตั้งค่ารหัสผ่านใหม่</Text>
+              <Text style={styles.textForgotPass}>{t('forgot_password')}</Text>
+              <Text style={styles.textForgotPass_2}>{t('contact_hr')}</Text>
             </View>
             <View style={styles.hrefViewTel}>
               <Pressable style={styles.buttonContact} onPress={() => Linking.openURL('tel:086-899-9089')}>
@@ -24,7 +28,7 @@ class ForgotPassword extends Component {
                   source={require('../assets/images/icon/phone.png')}
                 />
                 <View>
-                  <Text style={styles.textHref}>โทร</Text>
+                  <Text style={styles.textHref}>{t('tel')}</Text>
                   <Text style={styles.textHref_2}>086-899-9089</Text>
                 </View>
               </Pressable>
@@ -36,7 +40,7 @@ class ForgotPassword extends Component {
                   source={require('../assets/images/icon/email.png')}
                 />
                 <View style={styles.textViewEmail}>
-                  <Text style={styles.textHref}>อีเมล</Text>
+                  <Text style={styles.textHref}>{t('email')}</Text>
                   <Text style={styles.textHref_2}>human@email.com</Text>
                 </View>
               </Pressable>
@@ -45,56 +49,12 @@ class ForgotPassword extends Component {
           <View style={ComponentsStyle.viewStyle_2}>
             <View style={styles.viewButton}>
               <Pressable style={ComponentsStyle.button} onPress={() => this.props.navigation.goBack()}>
-                <Text style={ComponentsStyle.textButton}>กลับ</Text>
+                <Text style={ComponentsStyle.textButton}>{t('go_back')}</Text>
               </Pressable>
             </View>
           </View>
         </View>
       </SafeAreaView>
-      /*  <View style={{ flex: 1, padding: 10, backgroundColor: "#fff" }}>
-         <View>
-           <View style={styles.forgotPasswordView}>
-             <Image
-               style={styles.entryImage}
-               source={require('../assets/images/icon/contextual.png')}
-             />
-             <Text style={styles.textForgotPass}>ลืมรหัสผ่าน</Text>
-           </View>
-           <View style={styles.areaText}>
-             <Text style={styles.textForgotPass_2}>ติดต่อฝ่ายบุคคลบริษัทของคุณ{"\n"}เพื่อทำการตั้งค่ารหัสผ่านใหม่</Text>
-           </View>
-           <View style={styles.hrefView}>
-             <Pressable style={styles.buttonContact} onPress={() => Linking.openURL('tel:086-899-9089')}>
-               <Image
-                 style={{ marginTop: 10, marginRight: 10 }}
-                 source={require('../assets/images/icon/phone.png')}
-               />
-               <View>
-                 <Text style={styles.textHref}>โทร</Text>
-                 <Text style={styles.textForgotPass_2}>086-899-9089</Text>
-               </View>
-             </Pressable>
-           </View>
-           <View style={styles.hrefView}>
-             <Pressable style={styles.buttonContact} onPress={() => Linking.openURL('mailto:test@email.com')}>
-               <Image
-                 style={{ marginTop: 10, marginRight: 10 }}
-                 source={require('../assets/images/icon/email.png')}
-               />
-               <View><Text style={styles.textHref}>อีเมล</Text>
-                 <Text style={styles.textForgotPass_2}>test@email.com</Text>
-               </View>
-             </Pressable>
-           </View>
-         </View>
-         <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 20 }}>
-           <View style={styles.buttonView}>
-             <Pressable style={styles.buttonNext} onPress={() => this.props.navigation.goBack()}>
-               <Text style={styles.textNext}>กลับ</Text>
-             </Pressable>
-           </View>
-         </View>
-       </View> */
     )
   }
 }
@@ -160,4 +120,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default ForgotPassword;
+export default (withTranslation()(ForgotPassword));
