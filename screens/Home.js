da0a13f89fab2cd4154aca5327e3fd0caac54294
THANETNove <Novel_Manga@outlook.co.th>
2023-01-30 09:13:58 +0700

    icon wi

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Pressable, SafeAreaView, Image, ScrollView, TouchableOpacity, TextInput, Text, Linking, KeyboardAvoidingView, Platform, Dimensions, Modal, InputAccessoryView, Keyboard } from 'react-native';
+import { View, StyleSheet, Pressable, SafeAreaView, Image, ScrollView, StatusBar, statusBarStyle, statusBarTransition, hidden, TouchableOpacity, TextInput, Text, Linking, KeyboardAvoidingView, Platform, Dimensions, Modal, InputAccessoryView, Keyboard } from 'react-native';
 export default class Home extends Component {
     render() {
         return (
-            <Pressable /* style={ComponentsStyle.button} */ onPress={() => this.props.navigation.navigate("Page1")} >
-                <Text /* style={ComponentsStyle.textButton} */>home </Text>
-            </Pressable>
+            <View>
+                <StatusBar
+                /*   animated={false}
+                  backgroundColor="blue"
+                  /*  barStyle="default" */
+                /*  showHideTransition={statusBarTransition} */
+                /*   hidden={hidden} */
+                />
+                <Pressable /* style={ComponentsStyle.button} */ onPress={() => this.props.navigation.navigate("Page1")} >
+                    <Text /* style={ComponentsStyle.textButton} */>home </Text>
+                </Pressable>
+            </View>
 
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        /*         backgroundColor: 'red', */
+    },
+    buttonsContainer: {
+        padding: 10,
+    },
+    textStyle: {
+        textAlign: 'center',
+        marginBottom: 8,
+    },
+});
