bcd6d5ba9703fea55b60a905ea89dc898c184d59
THANETNove <Novel_Manga@outlook.co.th>
2023-02-07 18:25:57 +0700

    เเก้ navigation pop

--- navigation/NutritionStackScreen.js
+++ navigation/NutritionStackScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, View, Image, TouchableOpacity, Dimensions, StatusBar, HeaderBackButton } from 'react-native';
 import { createStackNavigator, CardStyleInterpolators, TransitionSpecs, TransitionPresets } from '@react-navigation/stack';
 import '../languages/i18n'; //ใช้สำหรับ 2ภาษา
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { useRoute } from '@react-navigation/native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import colors from '../constants/colors';
@@ -11,18 +12,21 @@ import History from '../screens/Nutrition/History';
 import QuizAnswer from '../screens/Nutrition/QuizAnswer';
 import Quiz from '../screens/Nutrition/Quiz';
 
-const StackNutrition = createStackNavigator();
 
+
+const Tab = createStackNavigator();
+const StackNutrition = createStackNavigator();
 function NutritionStackScreen() {
     const navigation = useNavigation();
+    const route = useRoute();
+
     return (
         <StackNutrition.Navigator >
             <StackNutrition.Screen name="Nutrition" component={Nutrition} options={{
                 headerShown: false
             }} />
-
             <StackNutrition.Screen name="History" component={History}
-                options={({
+                options={({ route, navigation }) => ({
                     title: "",
                     headerStyle: {
                         backgroundColor: colors.grey7,
@@ -30,9 +34,30 @@ function NutritionStackScreen() {
                     showLabel: false,
                     headerLeft: () => (
                         <View style={{ marginLeft: 16 }}>
-                            <TouchableOpacity onPress={() => navigation.popToTop()}>
+                            <TouchableOpacity onPress={() => navigation.pop()}>
                                 <Image
                                     source={require('../assets/images/icon/caret.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    ),
+
+                })}
+
+            />
+            <StackNutrition.Screen name="Successful" component={Successful}
+                options={({ route, navigation }) => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: colors.persianBlue,
+                    },
+                    headerShadowVisible: false,
+                    showLabel: false,
+                    headerLeft: () => (
+                        <View style={{ marginLeft: 16 }}>
+                            <TouchableOpacity onPress={() => navigation.pop()}>
+                                <Image
+                                    source={require('../assets/images/icon/chevron.png')}
                                 />
                             </TouchableOpacity>
                         </View>
@@ -40,30 +65,11 @@ function NutritionStackScreen() {
                 })}
             />
 
-            <StackNutrition.Screen name="Successful" component={Successful}
-                options={{
-                    title: "",
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: colors.persianBlue,
-                    },
-
-                    headerLeft: ({ props }) => (
-                        <View style={{ marginLeft: 16 }}>
-                            <TouchableOpacity {...props} onPress={() => navigation.pop()}>
-
-                                <Image
-                                    source={require('../assets/images/icon/chevron.png')}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                    ),
 
 
 
-                }} />
             <StackNutrition.Screen name="QuizAnswer" component={QuizAnswer}
-                options={({
+                options={({ route, navigation }) => ({
                     title: "",
                     headerStyle: {
                         backgroundColor: colors.grey7,
@@ -81,7 +87,7 @@ function NutritionStackScreen() {
                 })}
             />
             <StackNutrition.Screen name="Quiz" component={Quiz}
-                options={({
+                options={({ route, navigation }) => ({
                     title: "",
                     headerStyle: {
                         backgroundColor: colors.grey7,
@@ -98,7 +104,6 @@ function NutritionStackScreen() {
                     ),
                 })}
             />
-
 
         </StackNutrition.Navigator>
     );
