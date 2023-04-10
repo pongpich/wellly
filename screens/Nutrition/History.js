f3cceae01458a0f9b5c8e7db267536ace367b269
THANETNove <Novel_Manga@outlook.co.th>
2023-04-10 19:58:42 +0700

    เเก้ id

--- screens/Nutrition/History.js
+++ screens/Nutrition/History.js
@@ -28,7 +28,7 @@ class History extends Component {
                         {
                             nutrition_activity && nutrition_activity.map((item, i) => (
                                 //ส่ง params ผ่าน route
-                                <Pressable onPress={() => this.props.navigation.navigate("ArticleTemplate", { id: i + 1, mission_id: item.mission_id, heading: item.heading })} key={i + "fee"}>
+                                <Pressable onPress={() => this.props.navigation.navigate("ArticleTemplate", { id: item.week_in_program, mission_id: item.mission_id, heading: item.heading })} key={i + "fee"}>
                                     <View key={i} style={styles.row}>
                                         <View style={styles.numberView}>
                                             <Text style={styles.number}>{item.week_in_program}</Text>
@@ -48,7 +48,7 @@ class History extends Component {
                         }
                     </ScrollView>
                 </View>
-            </SafeAreaView>
+            </SafeAreaView >
         )
     }
 }
