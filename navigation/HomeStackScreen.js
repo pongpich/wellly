eaaca540653bf076afeb1b0ff8c3a77f0dca3ec8
Akkewach Yodsomboon <>
2023-04-11 12:08:57 +0700

    แก้บัคหน้า home รวนๆ

--- navigation/HomeStackScreen.js
+++ navigation/HomeStackScreen.js
@@ -10,12 +10,19 @@ import ArticleTemplate from '../screens/Nutrition/ArticleTemplate';
 import ExArticleTemplate from '../screens/Exercise/ExArticleTemplate';
 import Add from '../screens/Add/Add';
 import AddActivity from '../screens/Add/AddActivity';
+import Report from '../screens/Nutrition/Report';
+import ConfirmSubmit from '../screens/Nutrition/ConfirmSubmit';
+import ReportFeedback from '../screens/Nutrition/ReportFeedback';
+import Quiz from '../screens/Nutrition/Quiz';
+import QuizAnswer from '../screens/Nutrition/QuizAnswer';
+import { useSelector, useDispatch } from "react-redux";
 
 
 const HomeStack = createStackNavigator();
 
 function HomeStackScreen() {
     const navigation = useNavigation();
+    const routename = useSelector((state) => state.personalDataUser.route_name);
     return (
         <HomeStack.Navigator>
             <HomeStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
@@ -23,6 +30,93 @@ function HomeStackScreen() {
             <HomeStack.Screen name="ExArticleTemplate" component={ExArticleTemplate} options={{ headerShown: false }} />
             <HomeStack.Screen name="Add" component={Add} options={{ cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS, headerShown: false }} />
             <HomeStack.Screen name="AddActivity" component={AddActivity} options={{ cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS, headerShown: false }} />
+            <HomeStack.Screen name="QuizAnswer" component={QuizAnswer}
+                options={({ route, navigation }) => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: colors.white,
+                    },
+                    showLabel: false,
+                    showLabel: false,
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        return (
+                            routename != null ?
+                                routename.route_name === "Quiz" ?
+                                    <View style={{ marginLeft: 16 }}>
+                                        <TouchableOpacity onPress={() => navigation.dispatch(StackActions.replace('Home'))}>
+                                            < Image
+                                                source={require('../assets/images/icon/caret.png')}
+                                            />
+                                        </TouchableOpacity>
+                                    </View>
+                                    :
+                                    <View style={{ marginLeft: 16 }}>
+                                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                                            <Image
+                                                source={require('../assets/images/icon/caret.png')}
+                                            />
+                                        </TouchableOpacity>
+                                    </View>
+                                :
+                                <View style={{ marginLeft: 16 }}>
+                                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                                        <Image
+                                            source={require('../assets/images/icon/caret.png')}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                        )
+                    }
+                })}
+            />
+            < HomeStack.Screen name="Quiz" component={Quiz}
+                options={({ route, navigation }) => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: colors.white,
+                    },
+                    showLabel: false,
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <View style={{ marginLeft: 16 }}>
+                            <TouchableOpacity onPress={() => navigation.pop()}>
+                                <Image
+                                    source={require('../assets/images/icon/caret.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    ),
+
+                })}
+            />
+            <HomeStack.Screen name="Report" component={Report}
+                options={({ route, navigation }) => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: colors.white,
+                    },
+                    showLabel: false,
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <View style={{ marginLeft: 16 }}>
+                            <TouchableOpacity onPress={() => navigation.pop()}>
+                                <Image
+                                    source={require('../assets/images/icon/caret.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    ),
+
+                })}
+            />
+            <HomeStack.Screen name="ConfirmSubmit" component={ConfirmSubmit}
+                options={{ headerShown: false }}
+            />
+            <HomeStack.Screen name="ReportFeedback" component={ReportFeedback}
+                options={{ headerShown: false }}
+            />
+
         </HomeStack.Navigator>
     );
 }
