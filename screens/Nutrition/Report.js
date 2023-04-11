19718c6cb21df451a3c7671852e20b12fcee8809
THANETNove <Novel_Manga@outlook.co.th>
2023-04-11 10:36:09 +0700

    ปรับขนาดภาพ

--- screens/Nutrition/Report.js
+++ screens/Nutrition/Report.js
@@ -599,7 +599,7 @@ const styles = StyleSheet.create({
         marginTop: 16,
         marginBottom: 16,
         width: (deviceHeight > 1023) ? "100%" : "100%",
-        height: (deviceHeight > 1023) ? 500 : 300
+        height: (deviceHeight > 1023) ? 500 : 290
     },
 });
 
