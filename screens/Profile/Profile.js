cb90adc9dc10e4adc0281a37f91cefdec8431d06
Akkewach Yodsomboon <>
2023-05-03 21:08:44 +0700

    แก้คำผิด

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -281,7 +281,7 @@ class Profile extends Component {
                                             />
                                             <View style={{ flexDirection: "row" }}>
                                                 <Text style={styles.manuName}>ภาษา</Text>
-                                                <Text style={{ marginLeft: 10, marginTop: 5, color: colors.grey4 }}>England</Text>
+                                                <Text style={{ marginLeft: 10, marginTop: 5, color: colors.grey4 }}>English</Text>
                                             </View>
                                         </View>
                                         <Image
