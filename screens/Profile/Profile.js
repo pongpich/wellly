7ab224ae79bef848e5765f8dcd969f9caea4d40d
Akkewach Yodsomboon <>
2023-05-29 14:33:21 +0700

    ปรับเลขเวอร์ชั่น

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -358,7 +358,7 @@ class Profile extends Component {
                             </View>
                         </Pressable>
                     </View>
-                    <Text style={styles.version}>เวอร์ชั่น 1.5.1</Text>
+                    <Text style={styles.version}>เวอร์ชั่น 1.6</Text>
                 </ScrollView >
                 <View View style={styles.centeredView} >
                     <Modal isVisible={popupDeleteAccShow}
