4e6bb71704dd065b3b721743dbe1f6e5dbd82687
Akkewach Yodsomboon <>
2023-06-08 20:42:28 +0700

    ปรับคำว่าเวอร์ชั่น

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -358,7 +358,7 @@ class Profile extends Component {
                             </View>
                         </Pressable>
                     </View>
-                    <Text style={styles.version}>เวอร์ชั่น 1.6</Text>
+                    <Text style={styles.version}>{t('version')} 1.7</Text>
                 </ScrollView >
                 <View View style={styles.centeredView} >
                     <Modal isVisible={popupDeleteAccShow}
