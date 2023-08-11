a3b0c8b37dd848bee80ebf7ac89fcd45fb3fa39d
Akkewach Yodsomboon <>
2023-08-11 21:29:07 +0700

    เพิ่มเช็ค permission

--- screens/TestGPS.js
+++ screens/TestGPS.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { View, StyleSheet, Pressable, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Pressable, Text, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { useNavigation } from '@react-navigation/native';
+import * as Permissions from 'expo-permissions';
 
 const TestGPS = () => {
     const navigation = useNavigation();
+    const [locationPermission, setLocationPermission] = useState(null);
 
-    const CHROME_USER_AGENT =
-        'Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36';
+    useEffect(() => {
+        getLocationPermission();
+    }, []);
+
+    const getLocationPermission = async () => {
+        const { status } = await Permissions.askAsync(Permissions.LOCATION);
+        setLocationPermission(status);
+    };
+
+    
+
     return (
         <View style={styles.container}>
             <Pressable onPress={() => navigation.navigate('Home')}>
@@ -28,9 +39,15 @@ const TestGPS = () => {
             <Pressable onPress={() => navigation.navigate('Home')}>
                 <Text style={styles.reportChallenge}>{`WELLLY APP (WEB VIEW PAGE)`}</Text>
             </Pressable>
+
+
+            <View>
+                <Text>Location Permission: {locationPermission}</Text>
+                <Button title="Get Location" onPress={getLocationPermission} />
+            </View>
+
             <WebView
                 source={{ uri: 'https://platform.bebefitroutine.com/#/test_gps' }}
-                userAgent={CHROME_USER_AGENT}
             />
         </View>
     );
