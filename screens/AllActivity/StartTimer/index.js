9f8363844566e71396e12129803869b88c2c7f18
THANETNove <novel2020manga@gmail.com>
2024-05-06 15:43:18 +0700

    css

--- screens/AllActivity/StartTimer/index.js
+++ screens/AllActivity/StartTimer/index.js
@@ -4,11 +4,14 @@ import {
   Button,
   StyleSheet,
   ImageBackground,
+  Modal, Alert, Pressable,
   Image,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import * as GoogleSignIn from "expo-auth-session/providers/google";
 import IconRun from "../../../assets/images/icon/run.png";
+import IconStop from "../../../assets/images/icon/stop.png";
+import Contextual from "../../../assets/images/icon/Contextual2.png";
 import colors from "../../../constants/colors";
 
 const iosKey =
@@ -21,6 +24,9 @@ const webClientExpoKey =
 const StartTime = () => {
   const [stepCount, setStepCount] = useState(0);
   const [distance, setDistance] = useState(0);
+  const [statusStop, setStatusStop] = useState(true);
+  const [modalVisible, setModalVisible] = useState(true);
+
   const [req, res, promptAsync] = GoogleSignIn.useAuthRequest({
     androidClientId: androidkey,
     iosClientId: iosKey,
@@ -101,7 +107,9 @@ const StartTime = () => {
   }, [res]);
 
   return (
-    <View style={styles.container}>
+
+
+    <View style={styles.container} source={IconRun} >
       <View style={styles.boxTime}>
         <Text style={styles.textTime}>เวลา</Text>
         <Text style={styles.times}>00:12:45</Text>
@@ -120,13 +128,61 @@ const StartTime = () => {
       <View style={styles.boxImage}>
         <Image style={styles.image} source={IconRun} />
       </View>
-    </View>
+      <View style={styles.boxSop}>
+        {statusStop == true ?
+          <Image style={styles.stop} source={IconStop} />
+          :
+          <>
+            <View style={styles.circle}>
+              <View style={styles.circlePlay}>
+                <Text style={styles.textPlay}>เล่นต่อ</Text>
+              </View>
+              <View style={styles.circleFinish}>
+                <Text style={styles.textFinish}>เสร็จ</Text>
+              </View>
+            </View>
+
+            <Pressable onPress={() => setModalVisible(true)}>
+              <Text style={styles.abandon}>ละทิ้ง</Text>
+            </Pressable>
+
+          </>
+        }
+
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Image style={styles.contextual} source={Contextual} />
+            <Text style={styles.modalText}>ยืนยันการละทิ้งกิจกรรม</Text>
+            <View style={styles.modalBoxButton}>
+              <View style={styles.buttonBack}>
+                <Text style={styles.buttonTextBack}>กลับ</Text>
+              </View>
+              <View style={styles.buttonAbandon}>
+                <Text style={styles.buttonTextAbandon}>ละทิ้ง</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View >
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "red"
   },
   boxTime: {
     zIndex: 1,
@@ -163,7 +219,9 @@ const styles = StyleSheet.create({
     zIndex: 0,
     flex: 1,
     alignItems: "center", // จัดการให้ content อยู่กึ่งกลางตามแนวแกน X
-    justifyContent: "flex-end",
+    bottom: 64,
+
+
   },
   image: {
     zIndex: 0,
@@ -171,6 +229,130 @@ const styles = StyleSheet.create({
     maxWidth: 438,
     maxHeight: 559,
     resizeMode: "contain",
+
   },
+  boxSop: {
+    zIndex: 10,
+    flex: 1,
+    marginBottom: 35,
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  circle: {
+    zIndex: 2,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  circlePlay: {
+    zIndex: 2,
+    width: 80,
+    height: 80,
+    borderRadius: 50, // ค่านี้จะทำให้มันเป็นวงกลม 50% ของความกว้างหรือความสูง
+    backgroundColor: colors.white, // เปลี่ยนสีตามที่ต้องการ
+    borderColor: colors.secondary_MayaBlue, // เปลี่ยนสีขอบตามที่ต้องการ
+    borderWidth: 2, // เปลี่ยนขนาดของเส้นขอบตามต้องการ
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textPlay: {
+    color: colors.secondary_MayaBlue,
+    fontSize: 20,
+    fontFamily: "IBMPlexSansThai-Bold",
+  },
+  circleFinish: {
+    zIndex: 2,
+    marginLeft: 16,
+    width: 80,
+    height: 80,
+    borderRadius: 50, // ค่านี้จะทำให้มันเป็นวงกลม 50% ของความกว้างหรือความสูง
+    backgroundColor: colors.secondary_MayaBlue, // เปลี่ยนสีตามที่ต้องการ
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textFinish: {
+    color: colors.white,
+    fontSize: 20,
+    fontFamily: "IBMPlexSansThai-Bold",
+  },
+  stop: {
+    width: 80,
+    height: 80
+  },
+  abandon: {
+    marginTop: 8,
+    color: colors.grey2,
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai-Bold",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "flex-end", // ให้อยู่ชิดด้านล่าง
+
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+
+    alignItems: "center",
+    paddingTop: 32,
+    paddingBottom: 40,
+    marginBottom: 0,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  contextual: {
+    width: 120,
+    height: 120
+  },
+  modalText: {
+    marginTop: 24,
+    color: colors.black,
+    fontSize: 20,
+    fontFamily: "IBMPlexSansThai-Bold",
+  },
+  modalBoxButton: {
+    marginTop: 32,
+    flexDirection: "row",
+  },
+  buttonBack: {
+    width: 156,
+    height: 48,
+    backgroundColor: colors.white,
+    borderRadius: 50,
+    borderColor: colors.primary, // เปลี่ยนสีขอบตามที่ต้องการ
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center"
+
+  },
+  buttonAbandon: {
+    marginLeft: 16,
+    width: 155,
+    height: 48,
+    backgroundColor: colors.primary,
+    borderRadius: 50,
+    borderColor: colors.primary, // เปลี่ยนสีขอบตามที่ต้องการ
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonTextAbandon: {
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai-Bold",
+  },
+  buttonTextBack: {
+    color: colors.primary,
+    fontSize: 16,
+    fontFamily: "IBMPlexSansThai-Bold",
+  }
 });
 export default StartTime;
